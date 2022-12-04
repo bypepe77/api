67601ba0e4e8a114d05b75aba165b90c681e354e
Mario <mario.espada77@gmail.com>
2022-12-04 02:00:53 +0100

    Delete gorm connection

--- internal/common/database/dbConnection.go
+++ internal/common/database/dbConnection.go
@@ -7,29 +7,8 @@ import (
 	"os"
 
 	"github.com/bypepe77/api/ent"
-	"github.com/bypepe77/api/internal/common/models"
 	_ "github.com/go-sql-driver/mysql"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
-
-func DatabaseConectionGorm() gorm.DB {
-	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("MYSQL_USERNAME"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_PORT"),
-		os.Getenv("MYSQL_DATABASE"))
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	db.AutoMigrate(&models.Post{}, &models.User{}, &models.Comment{}, &models.Follow{})
-
-	return *db
-}
 
 func DatabaseConnectionEnt() *ent.Client {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True",
